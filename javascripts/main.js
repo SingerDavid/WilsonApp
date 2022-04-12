63004b71713f91a02ec657c2f2717c84350a3084
SingerDavid <dsinger1@hawk.iit.edu>
2022-04-12 11:59:32 -0500

    User data is transcribed into the fitters page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,50 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  'use strict';
 
-function handleSubmit(event) {
-    event.preventDefault();
+  function handleSubmit(event) {
+      event.preventDefault();
 
-    const data = new FormData(event.target);
+      const data = new FormData(event.target);
 
-    const value = Object.fromEntries(data.entries());
+      const value = Object.fromEntries(data.entries());
 
-    console.log({ value });
-  }
+      console.log(data);
+      console.log(value);
 
-  const form = document.querySelector('form');
-  form.addEventListener('submit', handleSubmit);
+      const newValue = JSON.stringify(value);
+      console.log(newValue);
+
+
+      fetch('http://localhost:8080/fitter/index.html')
+        .then(res => res.text())
+        .then((responseText) => {
+          const doc = new DOMParser().parseFromString(responseText, 'text/html');
+          console.log(doc)
+          let container = doc.querySelector('#container h2');
+          let ul = document.createElement('ul')
+          container.appendChild(ul);
+
+          let liParent = doc.querySelector('#container ul');
+
+          console.log('test value:', value);
+
+          Object.keys(value).forEach(item => {
+            let li = document.createElement('li')
+            li.classList.add('userData');
+            li.innerHTML = item + value[item];
+            console.log(item, value[item]);
+            // let name = doc.createElement('li').innerHTML = 'Name: option2' + item.clientName;
+            liParent.appendChild(li);
+          });
+
+      });//end fetch response
+
+    }
+
+    if (document.getElementById('clientPage')){
+      console.log('This is executing the client page');
+      const form = document.querySelector('form');
+      form.addEventListener('submit', handleSubmit);
+    }
+
+});
